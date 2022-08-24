c729bdd990d8ed90ac66d50cc3f3b6e5bec3bfbc
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-24 15:30:08 +0300

    task_36

--- hw/Program.cs
+++ hw/Program.cs
@@ -8,23 +8,32 @@
     return arr;
 }
 
-int amount(int[] arr)
+int Sum(int[] arr)
 {
-    int a = 0;
-    foreach (int el in arr)
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i += 2)
     {
-        a += (el % 2) == 0 ? 1 : 0;
+        sum += arr[i];
     }
-    return a;
+    return sum;
 }
 
 Console.Write("Enter the number of array elements: ");
 int num = int.Parse(Console.ReadLine());
-if (num <= 0) goto Close;
+if (num <= 0) goto Close2;
+Console.Write("Enter the min value for array elements: ");
+int Amin = int.Parse(Console.ReadLine());
+Console.Write("Enter the max value for array elements: ");
+int Amax = int.Parse(Console.ReadLine());
+if (Amin > Amax) goto Close1;
 
-int[] mas = Mas(num, 100, 1000);
+int[] mas = Mas(num, Amin, Amax);
 Console.WriteLine($"The array: [{String.Join(" ", mas)}]");
-Console.WriteLine($"The number of even numbers in the array is: {amount(mas)}");
+Console.WriteLine($"The sum of elements on odd positions is: {Sum(mas)}");
 
-Close:
-if (num <= 0) Console.WriteLine("Unable to create an array. The number must be greater than 0!");
+Close1:
+if (Amin > Amax) Console.WriteLine("Unable to create an array!");
+
+Close2:
+if (num <= 0) Console.WriteLine("Unable to create an array!");
+
