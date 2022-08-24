1239d9f955860fe8a9828b2480f031fb7461b75f
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-24 21:55:47 +0300

    task_star

--- hw/Program.cs
+++ hw/Program.cs
@@ -1,48 +1,46 @@
-﻿double[] Mas(int size, int min, int max)
+﻿int[] Mas(int size, int min, int max)
 {
-    double[] arr = new double[size];
+    int[] arr = new int[size];
     for (int i = 0; i < size; i++)
     {
-        arr[i] = Math.Round((new Random().Next(min, max) + new Random().NextDouble()), 2);
+        arr[i] = new Random().Next(min, max);
     }
     return arr;
 }
 
-double Min(double[] arr)
+int[] NewMas(int[] arr)
 {
-    double min = arr[0];
-    for (int i = 0; i < arr.Length; i++)
+    for (int i = 1; i < arr.Length; i++)
     {
-        if (arr[i] < min)
+        for (int j = 0; j < arr.Length - i; j++)
         {
-            min = arr[i];
+            if(arr[j] < arr[j + 1]) 
+            {
+                (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+            }
         }
     }
-    return min;
-}
-
-double Max(double[] arr)
-{
-    double max = arr[0];
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (arr[i] > max)
-        {
-            max = arr[i];
-        }
-    }
-    return max;
+    return arr;
 }
 
 Console.Write("Enter the number of array elements: ");
 int num = int.Parse(Console.ReadLine());
-if (num <= 0) goto Close;
+if (num <= 0) goto Close2;
+Console.Write("Enter the min value for array elements: ");
+int Amin = int.Parse(Console.ReadLine());
+Console.Write("Enter the max value for array elements: ");
+int Amax = int.Parse(Console.ReadLine());
+if (Amin > Amax) goto Close1;
 
-double[] mas = Mas(num, 0, 100);
-Console.WriteLine($"The array: [{String.Join("|", mas)}]");
-Console.WriteLine($"The min is: {Min(mas)}");
-Console.WriteLine($"The max is: {Max(mas)}");
-Console.WriteLine($"The diff is: {Max(mas) - Min(mas)}");
+int[] mas = Mas(num, Amin, Amax);
+Console.WriteLine($"The array: [{String.Join(" ", mas)}]");
+int[] mas1 = NewMas(mas);
 
-Close:
-if (num <= 0) Console.WriteLine("Unable to create an array. The number must be greater than 0!");
+Console.WriteLine($"After sorting: [{String.Join(" ", mas1)}]");
+
+Close1:
+if (Amin > Amax) Console.WriteLine("Unable to create an array!");
+
+Close2:
+if (num <= 0) Console.WriteLine("Unable to create an array! Max must be greater then Min.");
+
