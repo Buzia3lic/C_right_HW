3ec60e68a8a9aa2f605073281ff5785f3931aebe
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-08-31 17:43:18 +0300

    task_star_7_almost

--- hw/Program.cs
+++ hw/Program.cs
@@ -1,51 +1,78 @@
-﻿int[,] NewArray(int m, int n, int min, int max)
-{
-    int[,] arr = new int[m, n];
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            arr[i, j] = new Random().Next(min, max + 1);
-        } 
-    }
-    return arr;
-}
+﻿Console.Write("Enter a number in roman format: ");
+string number = Console.ReadLine();
 
-void PrintArray(int[,] arr)
+int ReplaceTo(string text)
 {
-    for (int i = 0; i < arr.GetLength(0); i++)
+    int[] numArray = new int[text.Length];
+
+    for (int i = 0; i < text.Length - 1; i++)
     {
-        for (int j = 0; j < arr.GetLength(1); j++)
+        if (text[i] == 'I' || text[i] == 'i')
+            if (text[i + 1] != 'i' && text[i + 1] != 'I' && text[i + 1] != 'V' && text[i + 1] != 'v' && text[i + 1] != 'X' && text[i + 1] != 'x') return 0;
+        if (text[i] == 'V' || text[i] == 'v')
+            if (text[i + 1] != 'i' && text[i + 1] != 'I') return 0;
+        if (text[i] == 'X' || text[i] == 'x')
+            if (text[i + 1] != 'x' && text[i + 1] != 'X' && text[i + 1] != 'L' && text[i + 1] != 'l' && text[i + 1] != 'I' && text[i + 1] != 'i' && text[i + 1] != 'C' && text[i + 1] != 'c' && text[i + 1] != 'V' && text[i + 1] != 'v') return 0;
+        if (text[i] == 'L' || text[i] == 'l')
+            if (text[i + 1] != 'i' && text[i + 1] != 'I' && text[i + 1] != 'V' && text[i + 1] != 'v' && text[i + 1] != 'X' && text[i + 1] != 'x') return 0;
+        if (text[i] == 'C' || text[i] == 'c')
+            if (text[i + 1] != 'i' && text[i + 1] != 'I' && text[i + 1] != 'C' && text[i + 1] != 'c' && text[i + 1] != 'L' && text[i + 1] != 'l' && text[i + 1] != 'X' && text[i + 1] != 'x' && text[i + 1] != 'V' && text[i + 1] != 'v' && text[i + 1] != 'D' && text[i + 1] != 'd' && text[i + 1] != 'M' && text[i + 1] != 'm') return 0;
+        if (text[i] == 'D' || text[i] == 'd')
+            if (text[i + 1] != 'i' && text[i + 1] != 'I' && text[i + 1] != 'C' && text[i + 1] != 'c' && text[i + 1] != 'L' && text[i + 1] != 'l' && text[i + 1] != 'X' && text[i + 1] != 'x' && text[i + 1] != 'V' && text[i + 1] != 'v') return 0;
+        if (text[i] == 'M' || text[i] == 'm')
+            if (text[i + 1] != 'i' && text[i + 1] != 'I' && text[i + 1] != 'D' && text[i + 1] != 'd' && text[i + 1] != 'C' && text[i + 1] != 'c' && text[i + 1] != 'L' && text[i + 1] != 'l' && text[i + 1] != 'X' && text[i + 1] != 'x' && text[i + 1] != 'V' && text[i + 1] != 'v' && text[i + 1] != 'M' && text[i + 1] != 'm') return 0;
+    }
+
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i] == 'I' || text[i] == 'i') numArray[i] = 1;
+        if (text[i] == 'V' || text[i] == 'v') numArray[i] = 5;
+        if (text[i] == 'X' || text[i] == 'x') numArray[i] = 10;
+        if (text[i] == 'L' || text[i] == 'l') numArray[i] = 50;
+        if (text[i] == 'C' || text[i] == 'c') numArray[i] = 100;
+        if (text[i] == 'D' || text[i] == 'd') numArray[i] = 500;
+        if (text[i] == 'M' || text[i] == 'm') numArray[i] = 1000;
+    }
+
+    for (int i = 0; i < numArray.Length; i++)
+    {
+        if (numArray[i] == 0)
         {
-            Console.Write($"|{arr[i, j]}");
+            return 0;
         }
-        Console.WriteLine("|");
     }
-}
 
-double[] GetAverageArray(int[,] arr)
-{
-    double[] result = new double[arr.GetLength(1)];
-    
-    for (int i = 0; i < result.Length; i++)
+    int result = numArray[numArray.Length - 1];
+    int count = 1;
+
+    for (int i = numArray.Length - 1; i > 0; i--)
     {
-        result[i] = 0;
-        for (int j = 0; j < arr.GetLength(0); j++)
+        if (numArray[i - 1] == numArray[i])
         {
-            result[i] += arr[j, i];
+            if (count == 3)
+            {
+                return 0;
+            }
+            else
+            {
+                result += numArray[i - 1];
+                count++;
+            }
         }
-        result[i] = Math.Round(result[i] /= arr.GetLength(0), 1);
-
+        else
+        {
+            count = 1;
+            if (numArray[i - 1] > numArray[i])
+            {
+                result += numArray[i - 1];
+            }
+            else result -= numArray[i - 1];
+        }
     }
-    
     return result;
 }
 
-Console.Write("Enter number of lines: ");
-int m = int.Parse(Console.ReadLine());
-Console.Write("Enter number of columns: ");
-int n = int.Parse(Console.ReadLine());
-int[,] newMas = NewArray(m, n, 0, 9); //я не стал делать ввод мин и макс, так как по условию этого нет
-Console.WriteLine("The array: ");
-PrintArray(newMas);
-Console.WriteLine($"The averages fo each column is : [{String.Join("; ", GetAverageArray(newMas))}]");
+if (ReplaceTo(number) == 0 || ReplaceTo(number) > 3999) Console.WriteLine("There is a mistake in the roman number!");
+else Console.WriteLine(ReplaceTo(number));
+
+
