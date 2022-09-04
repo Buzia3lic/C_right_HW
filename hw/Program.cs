7dc644b1faca08d70a7cf572c617e30296a696b8
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-09-04 21:24:02 +0300

    task_star_HW8

--- hw/Program.cs
+++ hw/Program.cs
@@ -1,66 +1,21 @@
-int[,] GetArrey(int m, int n)
+int Sqrt(int num)
 {
-    int[,] result = new int[m, n];
-    int count = m * n;
-    int timer = 1;
-    for (int i = 0; count >= 0; i++)
+    double[] arr = new double[2];
+    arr[0] = num / 2;
+    double diff = 0.000001;
+    while((arr[0] - arr[1]) > diff)
     {
-        
-       
-        for (int j = i; j < result.GetLength(1) - i; j++)
-        {
-            result[i, j] = timer;
-            timer++;
-            count--;
-            if (count == 0) break;
-        }
-        if (count == 0) break;
-        for (int j = i; j < result.GetLength(0) - 2 - i; j++)
-        {
-            result[j + 1, result.GetLength(1) - 1 - i] = timer;
-            timer++;
-            count--;
-            if (count == 0) break;
-        }
-        if (count == 0) break;
-        for (int j = result.GetLength(1) - 1 - i; j >= i; j--)
-        {
-            result[result.GetLength(0) - 1 - i, j] = timer;
-            timer++;
-            count--;
-        }
-        if (count == 0) break;
-        for (int j = result.GetLength(0) - 2 - i; j > i; j--)
-        {
-            result[j, i] = timer;
-            timer++;
-            count--;
-            if (count == 0) break;
-        }
-        if (count == 0) break;
+        arr[0] = (arr[0] + num / arr[0]) / 2;
+        arr[1] = (arr[0] + num / arr[0]) / 2;
     }
-
-
+    int result = (int)arr[1];
     return result;
+
 }
 
-void PrintArr(int[,] arr)
-{
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            if (arr[i, j] < 10) Console.Write($"{arr[i, j]}  ");
-            else Console.Write($"{arr[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-}
+Console.Write("Enter a number: ");
+int num = int.Parse(Console.ReadLine());
 
-Console.Write("Enter M: ");
-int m = int.Parse(Console.ReadLine());
-Console.Write("Enter N: ");
-int n = int.Parse(Console.ReadLine());
+int result = Sqrt(num);
+Console.WriteLine($"The square root of a number {num} is: {result}");
 
-int[,] theArray = GetArrey(m, n);
-PrintArr(theArray);
