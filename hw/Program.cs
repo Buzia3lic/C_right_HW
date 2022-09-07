130d0a8cef13aadfcd96dbfe8dd6a246e2de5f95
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-09-07 17:51:19 +0300

    task_68

--- hw/Program.cs
+++ hw/Program.cs
@@ -1,13 +1,17 @@
-int SumMtoN(int start, int num)
+double AckerFun(double m, double n)
 {
-    if (start == num) return start;
-    return start + SumMtoN(start + 1, num);
+    if (m == 0) return n + 1;
+    if (m == 1) return n + 2;
+    if (m == 2) return 2 * n + 3;
+    if (m == 3) return Math.Pow (2, n + 3) - 3;
+    return Math.Pow (2, (AckerFun(m - 1, n)) + 3) - 3;
 }
 
-Console.Write("Enter N1: ");
-int num1 = int.Parse(Console.ReadLine());
-Console.Write("Enter N2: ");
-int num2 = int.Parse(Console.ReadLine());
+Console.Write("Enter m: ");
+double m = int.Parse(Console.ReadLine());
+Console.Write("Enter n: ");
+double n = int.Parse(Console.ReadLine());
+if (m < 0 || n < 0) Console.Write("Tne numbers mut be greater then 0!");
+else Console.Write(AckerFun(m, n));
 
-int sum = SumMtoN(num1, num2);
-Console.Write($"Sum of all numbers from {num1} to {num2} is: {sum}");
+
